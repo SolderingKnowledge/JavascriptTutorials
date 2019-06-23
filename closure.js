d94a033bd8a5637e8973010263458610a2bce3fd
SolderingKnowledge <imaikol@yahoo.com>
2019-06-22 21:54:05 -0700

    closure

--- closure.js
+++ closure.js
@@ -16,3 +16,24 @@ if(true){
   }
   
   f();//  Can't find variable: f -> because it created lexical scope and doesn't get hoisted
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+for(let i=0; i<3; i++){//creates lexical scope
+    setTimeout(()=>{
+        console.log(i);
+    }, 0)
+}
+
+console.log("after the time");//output: after the time &  0,1,2
+
+
+for(var i=0; i<3; i++){//gets hoisted and setTimeOut will get the last values of i
+    setTimeout(()=>{
+        console.log(i);
+    }, 0)
+}
+console.log("after the time");//output: after the time &  3,3,3
